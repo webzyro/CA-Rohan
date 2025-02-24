f5b42b5ff4eff3425adbaa622d33c45958f2545d
webzyro <webzyro@gmail.com>
2025-02-24 13:55:11 +0530

    Modified some pages

--- index.js
+++ index.js
@@ -13,6 +13,32 @@ $(document).ready(function () {
       '<i class="fas fa-chevron-right"></i>',
     ],
   });
+
+  // Team Carousel
+  $(".team-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    navText: [
+      '<i class="fas fa-chevron-left"></i>',
+      '<i class="fas fa-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 });
 
 // Stats counter animation
